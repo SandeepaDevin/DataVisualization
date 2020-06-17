95791ae72dd565dd4fe6dba844b6a9c592eb580c
SandeepaDevin <48104360+SandeepaDevin@users.noreply.github.com>
2020-06-17 13:37:28 +0530

    add piechart

--- PieChartScatterPlot.py
+++ PieChartScatterPlot.py
@@ -6,3 +6,39 @@ df_can = pd.read_excel('https://s3-api.us-geo.objectstorage.softlayer.net/cf-cou
                        skiprows=range(20),
                        skipfooter=2)
 df_can.head()
+df_can.drop(['AREA', 'REG', 'DEV', 'Type', 'Coverage'], axis=1, inplace=True)  # clean up the dataset to remove unnecessary columns (eg. REG)
+df_can.rename(columns={'OdName':'Country', 'AreaName':'Continent','RegName':'Region'}, inplace=True)# let's rename the columns so that they make sense
+df_can.columns = list(map(str, df_can.columns))# for sake of consistency, let's also make all column labels of type string
+df_can.set_index('Country', inplace=True)# set the country name as index - useful for quickly looking up countries using .loc method
+df_can['Total'] = df_can.sum(axis=1)# add total column
+years = list(map(str, range(1980, 2014))) # years that we will be using in this lesson - useful for plotting later on
+print('data dimensions:', df_can.shape)
+%matplotlib inline
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.style.use('ggplot') # optional: for ggplot-like style
+
+# check for latest version of Matplotlib
+print('Matplotlib version: ', mpl.__version__) # >= 2.0.0
+# group countries by continents and apply sum() function 
+df_continents = df_can.groupby('Continent', axis=0).sum()
+
+# note: the output of the groupby method is a `groupby' object. 
+# we can not use it further until we apply a function (eg .sum())
+print(type(df_can.groupby('Continent', axis=0)))
+
+df_continents.head()
+# autopct create %, start angle represent starting point
+df_continents['Total'].plot(kind='pie',
+                            figsize=(5, 6),
+                            autopct='%1.1f%%', # add in percentages
+                            startangle=90,     # start angle 90° (Africa)
+                            shadow=True,       # add shadow      
+                            )
+
+plt.title('Immigration to Canada by Continent [1980 - 2013]')
+plt.axis('equal') # Sets the pie chart to look like a circle.
+
+plt.show()
